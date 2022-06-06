0b7fc959f0dad82d8250f699f0cd672d49626749
Jorge Ochipinti <jorgeochipinti@Air-de-Jorge.fibertel.com.ar>
2022-06-06 13:48:51 -0300

    head

--- components/Layout/UserLayout.tsx
+++ components/Layout/UserLayout.tsx
@@ -6,6 +6,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { useRouter } from 'next/router';
 import { useContext } from "react";
 import { UiContext } from "../../context/ui";
+import Head from 'next/head';
 
 
 export const UserLayout = ({ children }: any) => {
@@ -13,54 +14,60 @@ export const UserLayout = ({ children }: any) => {
     const { toggleSideMenu } = useContext(UiContext)
 
     return (
-        <Box>
+        <>
+            <Head>
+            <meta property="og:image" content="https://res.cloudinary.com/dfcqgpkvs/image/upload/v1654480251/jqb5cetdrxmwagv5a1xn.png" />
+            <title>Jorge Ochipinti</title>
+            </Head>
+            <Box>
 
-            <SideMenu />
-            <AppBar position="fixed" >
-                <Toolbar >
+                <SideMenu />
+                <AppBar position="fixed" >
+                    <Toolbar >
 
-                    <Box sx={{ display: { xs: 'block', sm: 'none' }, mt: 1 }}
-                        className="fadeIn">
-                        <Link>
-                            <Button onClick={toggleSideMenu}>
-                                <MenuIcon />
-                            </Button>
-                        </Link>
-                    </Box>
-                    <Box flex={1} />
-
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}
-                        className="fadeIn">
-                        <NextLink href='/' passHref>
+                        <Box sx={{ display: { xs: 'block', sm: 'none' }, mt: 1 }}
+                            className="fadeIn">
                             <Link>
-                                <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/' ? 'white' : 'black' }} color={router.asPath == '/' ? 'primary' : 'info'} >Home</Button>
+                                <Button onClick={toggleSideMenu}>
+                                    <MenuIcon />
+                                </Button>
                             </Link>
-                        </NextLink>
-                        <NextLink href='/projects' passHref>
-                            <Link>
-                                <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/projects' ? 'white' : 'black' }} color={router.asPath == '/projects' ? 'primary' : 'info'} >Projects</Button>
-                            </Link>
-                        </NextLink>
-                        <NextLink href='/about' passHref>
-                            <Link>
-                                <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/about' ? 'white' : 'black' }} color={router.asPath == '/about' ? 'primary' : 'info'} >About</Button>
-                            </Link>
-                        </NextLink>
-                        <NextLink href='/cv' passHref>
-                            <Link>
-                                <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/cv' ? 'white' : 'black' }} color={router.asPath == '/cv' ? 'primary' : 'info'} >CV</Button>
-                            </Link>
-                        </NextLink>
-                    </Box>
+                        </Box>
+                        <Box flex={1} />
 
-                    <Box flex={1} />
-                    <Typography variant='body1' sx={{ color: 'black', display: { xs: 'block', sm: 'none', md: 'none' } }}>Jorge Ochipinti</Typography>
-                </Toolbar>
-            </AppBar>
+                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}
+                            className="fadeIn">
+                            <NextLink href='/' passHref>
+                                <Link>
+                                    <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/' ? 'white' : 'black' }} color={router.asPath == '/' ? 'primary' : 'info'} >Home</Button>
+                                </Link>
+                            </NextLink>
+                            <NextLink href='/projects' passHref>
+                                <Link>
+                                    <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/projects' ? 'white' : 'black' }} color={router.asPath == '/projects' ? 'primary' : 'info'} >Projects</Button>
+                                </Link>
+                            </NextLink>
+                            <NextLink href='/about' passHref>
+                                <Link>
+                                    <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/about' ? 'white' : 'black' }} color={router.asPath == '/about' ? 'primary' : 'info'} >About</Button>
+                                </Link>
+                            </NextLink>
+                            <NextLink href='/cv' passHref>
+                                <Link>
+                                    <Button variant='contained' sx={{ fontWeight: 700, m: 1, color: router.asPath == '/cv' ? 'white' : 'black' }} color={router.asPath == '/cv' ? 'primary' : 'info'} >CV</Button>
+                                </Link>
+                            </NextLink>
+                        </Box>
 
-            {children}
+                        <Box flex={1} />
+                        <Typography variant='body1' sx={{ color: 'black', display: { xs: 'block', sm: 'none', md: 'none' } }}>Jorge Ochipinti</Typography>
+                    </Toolbar>
+                </AppBar>
 
-            <Footer />
-        </Box>
+                {children}
+
+                <Footer />
+            </Box>
+        </>
     )
 }
