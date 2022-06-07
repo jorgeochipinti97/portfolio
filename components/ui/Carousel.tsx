f155e86da974bf4020f15b5f5127ce7859c40196
Jorge Ochipinti <jorgeochipinti@Air-de-Jorge.fibertel.com.ar>
2022-06-07 13:40:26 -0300

    s

--- components/ui/Carousel.tsx
+++ components/ui/Carousel.tsx
@@ -40,7 +40,7 @@ export const CarouselPage: FC<Props> = ({ data }) => {
             autoPlay={true}
             arrows={false}
             autoPlaySpeed={3000}
-            transitionDuration={800}
+            transitionDuration={1500}
 
 
         >
