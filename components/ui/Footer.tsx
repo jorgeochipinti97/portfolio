eab85e393f95d65acc26498f52c2433a6f09df7f
Jorge Ochipinti <jorgeochipinti@Air-de-Jorge.fibertel.com.ar>
2022-06-10 19:16:23 -0300

    s

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,15 +1,15 @@
 import { Box, Divider, Typography } from '@mui/material'
 import React from 'react'
-import MoodRoundedIcon from '@mui/icons-material/MoodRounded';import { Contact } from './Contact';
+import MoodRoundedIcon from '@mui/icons-material/MoodRounded'; import { Contact } from './Contact';
 export const Footer = () => {
     return (
-        <footer  style={{backgroundColor:'rgba(0, 0, 0, 1)'}}>
+        <footer style={{ backgroundColor: 'rgba(0, 0, 0, 1)' }}>
             <Box sx={{ pt: 5, pb: 5 }} >
                 <Divider sx={{ my: 1 }} />
                 <Contact />
                 <Box display='flex' justifyContent='center'>
                     <Box display='flex'>
-                <Typography variant='subtitle1' sx={{color:'aliceblue'}}>Thanks for coming. </Typography><MoodRoundedIcon/>
+                        <Typography variant='subtitle1' sx={{ color: 'aliceblue' }}>Thanks for coming. </Typography><MoodRoundedIcon sx={{ color: 'white' }} />
                     </Box>
                 </Box>
             </Box>
